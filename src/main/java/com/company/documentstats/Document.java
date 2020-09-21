8ba50e5df438f04d0aac167ad10fa8318bea620f
cymrucoder <git.cmt@gmail.com>
2020-09-21 19:53:33 +0100

    Add initial line counting method.

--- src/main/java/com/company/documentstats/Document.java
+++ src/main/java/com/company/documentstats/Document.java
@@ -1,6 +1,28 @@
 package com.company.documentstats;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Document {
 
+    String filePath;
+
+    public Document(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public int getLineCount() throws FileNotFoundException, IOException {
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        int lineCount = 0;
+
+        while ((br.readLine()) != null) {
+            lineCount++;
+        }
+
+        br.close();
+
+        return lineCount;
+    }
 }
--- src/test/java/com/company/documentstats/DocumentTest.java
+++ src/test/java/com/company/documentstats/DocumentTest.java
@@ -1,21 +1,26 @@
 package com.company.documentstats;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DocumentTest {
 
     Document document;
-    
+
     public DocumentTest() {
-        document = new Document();
+        document = new Document(getClass().getClassLoader().getResource("testdoc.txt").getPath());
     }
 
     @Test
-    public void testRead() {
-        File file = new File(getClass().getClassLoader().getResource("testdoc.txt").getFile());
-        assertTrue(file.exists());
+    public void testCountLines_whenFileExists_shouldReturnCorrectNumber() throws IOException {// TODO handle exceptions
+        assertEquals(document.getLineCount(), 6);
     }
 
 }
