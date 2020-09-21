4341fcc49f19acb2959a257eaca790ebefdf530a
cymrucoder <git.cmt@gmail.com>
2020-09-21 18:58:25 +0100

    Add reading test file and start README.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # doc-stats
 Generate some statistics about text documents.
+
+## Build
+tbd
+
+## Assumptions
+
+* ASCII alphanumeric ([a-zA-Z0-9]*) only (for now?)
+* Case is insensitive, count upper and lower case letters together
--- src/test/java/com/company/documentstats/DocumentTest.java
+++ src/test/java/com/company/documentstats/DocumentTest.java
@@ -1,16 +1,21 @@
 package com.company.documentstats;
 
+import java.io.File;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DocumentTest {
+
+    Document document;
     
     public DocumentTest() {
+        document = new Document();
     }
 
     @Test
-    public void testSomeMethod() {
-        fail();
+    public void testRead() {
+        File file = new File(getClass().getClassLoader().getResource("testdoc.txt").getFile());
+        assertTrue(file.exists());
     }
-    
+
 }
