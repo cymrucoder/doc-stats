6f21f6a43d9e35a2805c0b818733d316a8e19cba
cymrucoder <git.cmt@gmail.com>
2020-09-21 17:26:14 +0100

    Set up project.

---
+++ src/main/java/com/company/documentstats/Document.java
@@ -0,0 +1,6 @@
+package com.company.documentstats;
+
+
+public class Document {
+
+}
---
+++ src/test/java/com/company/documentstats/DocumentTest.java
@@ -0,0 +1,16 @@
+package com.company.documentstats;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DocumentTest {
+    
+    public DocumentTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        fail();
+    }
+    
+}
